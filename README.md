d21b5be3094695852d7cfefbcf2f2044550838e1
Eric Lundell <emlundell@alaska.edu>
2019-11-12 14:41:29 -0900

    More established pipeline. Beginning of html.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # nginx-webcast
-An attempt to make a webcasting platform and website
+An attempt to make a webcasting platform and website.
+
+# Webcast setup #
+
+## Local Build and Run ##
+
+### Setup action.conf ###
+
+Environment varibles to be used by `action.sh`.
+
+The `X_DOCKER_IP` is the bridge ip of the docker network, e.g. `172.17.0.1`.
+This can be found by `ip a | grep docker0 | grep init`.
+We could use `--network=host` on `docker run` but I rather have a locked down network from the start.
+https://docs.docker.com/v17.09/engine/userguide/networking/#default-networks  
+
+### Docker Container Actions ###
+
+```
+cd ./webcast
+
+# Build docker image
+source ./action.sh build
+
+# Start docker container running nginx  
+source ./action.sh start
+
+# Stop docker container running nginx
+source ./action.sh stop
+
+# Restart docker nginx container
+source ./action.sh restart
+```
+
+### Broadcast Stream ###
+
+While not the only way, I'm using OBS (Open Broadcast Software, https://obsproject.com/) to transmit an image to `rtmp://$X_DOCKER_IP/channel/`.
+
+### Watch Stream ###
+
+while not the only way, I'm using VLC to receive `rtmp://$X_DOCKER_IP/channel/<key>`.
--- webcast/action.sh
+++ webcast/action.sh
@@ -2,21 +2,40 @@
 
 ACT=$1
 
+# Export enviroment vars set in action.conf
+# https://unix.stackexchange.com/a/79077
+set -a
+. ./action.conf
+set +a
+
 if [[ $ACT == 'build' ]] ; then
 
-  echo "Building..."
-  docker build -t stake:latest -f dockerfile .
+  envsubst '$X_DOCKER_IP' < "nginx.conf.example" > "nginx.conf"
+  envsubst '$X_DOCKER_IP' < "dash.html.example" > "dash.html"
+
+  echo "Building docker with network '$X_DOCKER_IP'"
+  docker build -t nginx-webcast:latest -f dockerfile .
 
 elif [[ $ACT == 'start' ]]; then
 
   echo "Starting..."
-  export X_IMAGE_HASH=$(docker ps -lq)
+  export X_IMAGE_HASH=$(docker run -d -p 8080:8080 -p 1935:1935 --rm nginx-webcast:latest)
+  #export X_IMAGE_HASH=$(docker ps -lq)
   echo "Docker is running with hash '$X_IMAGE_HASH'"
 
 elif [[ $ACT == 'stop' ]]; then
 
   echo "Stopping $X_IMAGE_HASH"
   docker stop $X_IMAGE_HASH
+
+elif [[ $ACT == 'restart' ]]; then
+
+  echo "Stopping $X_IMAGE_HASH"
+  docker stop $X_IMAGE_HASH
+  echo "Starting..."
+  export X_IMAGE_HASH=$(docker run -d -p 8080:8080 -p 1935:1935 --rm nginx-webcast:latest)
+  #export X_IMAGE_HASH=$(docker ps -lq)
+  echo "Docker is running with hash '$X_IMAGE_HASH'"
 
 else
 
